7c62f86a620b256ff113bf15d80737ff5d3f7218
francisco-desktop <jose_fko_92@hotmail.com>
2022-11-09 22:15:01 -0600

    observer links

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
-const hiddenElemets = document.querySelectorAll(".hidden");
+const hiddenElemets = document.querySelectorAll('.hidden');
+const links = document.querySelectorAll('.link');
 
 const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        entry.target.classList.add("show");
+        entry.target.classList.add('show');
+
+        links.forEach( link => {
+          const href = link.getAttribute('href').split('#')[1];
+          const id = entry.target.id;
+
+          if( href === id){
+            link.classList.add("link--active")
+          }else{
+            link.classList.remove('link--active')
+          }
+        })
       } else {
-        entry.target.classList.remove("show");
+        entry.target.classList.remove('show');
       }
     });
   },
   {
-    threshold: 0.3,
+    threshold: 0.45,
   }
 );
 
